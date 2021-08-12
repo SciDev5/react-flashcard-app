3ecdf50fe85941c54580b3d305ed785cfadbea0b
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2021-08-12 09:21:08 -0400

    Add header, fix FlashcardPage styles

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from "react";
+import { Translation } from "react-i18next";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 export default class Header extends React.Component {
     render():ReactNode {
-        return (
-            <nav>
-                header lol
+        return (<Translation>{t=>(
+            <nav className="Header">
+                <Link to="/" className="-appName" aria-label={t("header.mainPageLink")}>{t("header.appName")}</Link>
+                <Link to="/">{t("header.mainPageLink")}</Link>
             </nav>
-        );
+        )}</Translation>);
     }
 }
