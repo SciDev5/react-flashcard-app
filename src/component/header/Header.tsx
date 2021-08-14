aed1b5b5a4ea2839b2d0729dbe4e71d196dfad7f
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2021-08-14 09:11:59 -0400

    Move header towards center for large screens.

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,15 +1,20 @@
 import React, { ReactNode } from "react";
 import { Translation } from "react-i18next";
 import { Link } from "react-router-dom";
+import WidthLimiter from "../leaf/WidthLimiter/WidthLimiter";
 import "./Header.scss";
 
 export default class Header extends React.Component {
     render():ReactNode {
         return (<Translation>{t=>(
-            <nav className="Header">
-                <Link to="/" className="-appName" aria-label={t("header.mainPageLink")}>{t("app.name")}</Link>
-                <Link to="/">{t("header.mainPageLink")}</Link>
-            </nav>
+            <div className="Header">
+                <WidthLimiter width={800}>
+                    <nav>
+                        <Link to="/" className="-appName" aria-label={t("header.mainPageLink")}>{t("app.name")}</Link>
+                        <Link to="/">{t("header.mainPageLink")}</Link>
+                    </nav>
+                </WidthLimiter>
+            </div>
         )}</Translation>);
     }
 }
